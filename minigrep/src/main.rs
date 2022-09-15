3f6e90ecc687c1d480893e7e6c0c869f4711a2f5
Eduardo Flores <eduardofv@gmail.com>
2022-09-15 13:34:05 -0500

    minigrep 12.3

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,33 +1,22 @@
 use std::env;
-use std::fs;
+use std::process;
 
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {err}");
+        process::exit(1);
+    });
 
     //dbg!(args);
     println!("q: {}", config.query);
     println!("path: {}", config.path);
 
-    let contents = fs::read_to_string(config.path)
-        .expect("Should have been able to read the file");
-
-    println!("Text:\n{contents}");
-}
-
-
-struct Config {
-    query: String,
-    path: String,
-}
-
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let path = args[2].clone();
-
-    let config = Config { query, path };
-    config
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
