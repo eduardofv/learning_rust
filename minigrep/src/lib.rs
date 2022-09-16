3c5aaefb570c3481572dc8b756eb83b039a4e5ba
Eduardo Flores <eduardofv@gmail.com>
2022-09-15 21:00:52 -0500

    mid 12.4

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,6 +2,34 @@ use std::error::Error;
 use std::fs;
 
 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.path)?;
+
+    println!("Text:\n{contents}");
+
+    println!("Results:");
+    let results = search(&config.query, &contents);
+    for result in results {
+        println!("{}", result);
+    }
+
+    Ok(())
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 pub struct Config {
     pub query: String,
     pub path: String,
@@ -23,9 +51,18 @@ impl Config {
 }
 
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.path)?;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("Text:\n{contents}");
-    Ok(())
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+            
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
