e5d6e5137a16b812abca232243d4004469b20953
Eduardo Flores <eduardofv@gmail.com>
2022-09-15 22:43:43 -0500

    12.6

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -8,7 +9,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Text:\n{contents}");
 
     println!("Results:");
-    let results = search(&config.query, &contents);
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
     for result in results {
         println!("{}", result);
     }
@@ -29,10 +36,23 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query_low = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query_low) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 pub struct Config {
     pub query: String,
     pub path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -45,7 +65,9 @@ impl Config {
         let query = args[1].clone();
         let path = args[2].clone();
 
-        Ok(Config { query, path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, path, ignore_case })
     }
 
 }
@@ -65,4 +87,31 @@ Pick three.";
             
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_sensitive_search() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+            
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive_search() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+            
+        assert_eq!(
+            vec!["safe, fast, productive.", "Duct tape"], 
+            search_case_insensitive(query, contents));
+    }
+
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {err}");
+        eprintln!("Error parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
     println!("path: {}", config.path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 }
