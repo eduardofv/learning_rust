e0227872e6c3325d39e660bb2791665969e5c2e9
Eduardo Flores <eduardofv@gmail.com>
2022-09-29 19:44:36 -0500

    13.2.2

--- iter2.rs
+++ iter2.rs
@@ -8,7 +8,14 @@ fn main() {
 
     println!("v1: {:?}", v1);
 
+    //aqui ya se consumio, por lo que ya no es valido
+    /*
     for val2 in v1_iter() {
         println!("{}", val2);
     }
+    */
+
+    let i2 = v1.iter();
+    let s: i32 = i2.sum();
+    println!("{}", s);
 }
