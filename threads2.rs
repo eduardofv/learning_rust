a9ebfd27aeb403d92c91132795d0a9b648cae9e4
Eduardo Flores <eduardofv@gmail.com>
2022-10-07 13:41:43 -0500

    Pruebas de threading

--- threads2.rs
+++ threads2.rs
@@ -2,17 +2,24 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let mut handles = vec![];
+
     for j in 1..3 {
-        thread::spawn(move || {
+        let handle = thread::spawn(move || {
             for i in 1..10 {
                 println!("In Subthread {} count {}", j, i);
                 thread::sleep(Duration::from_millis(1));
             }
         });
+        handles.push(handle);
     }
 
     for i in 1..5 {
         println!("In main thread count {i}");
         thread::sleep(Duration::from_millis(1));
+    }
+
+    for h in handles {
+        h.join().unwrap();
     }
 }
