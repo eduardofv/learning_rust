a744042cafe380188f1254fe70436a91591d14d6
Eduardo Flores <eduardofv@gmail.com>
2022-10-17 19:32:45 -0500

    Prueba de oop, ver comentarios en el lib

--- oop_my_test/src/lib.rs
+++ oop_my_test/src/lib.rs
@@ -1,3 +1,10 @@
+//ESTA ES UNA PRUEBA SOBRE BORROWS
+//  observar como cuando se lee el estado en Post.request_review en el grabbed state
+//  el estado Post.state queda en None
+//  Tambien como se implementa el debug en el trait
+
+use std::fmt::Debug;
+
 pub struct Post {
     state: Option<Box<dyn State>>,
     content: String,
@@ -20,8 +27,10 @@ impl Post {
     }
 
     pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take(() {
-            self.state = Some(s.request_review());
+        if let Some(s) = self.state.take() {
+            let grabbed_state = Some(s.request_review());
+            println!("Grabbed the state {:?}", grabbed_state);
+            println!("internal state is now {:?}", self.state);
         }
     }
 }
@@ -30,6 +39,13 @@ trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
 }
 
+impl Debug for dyn State {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "State")
+    }
+}
+
+#[derive(Debug)]
 struct Draft {}
 
 impl State for Draft {
@@ -38,6 +54,7 @@ impl State for Draft {
     }
 }
 
+#[derive(Debug)]
 struct PendingReview {}
 
 impl State for PendingReview {
---
+++ oop_my_test/src/main.rs
@@ -0,0 +1,7 @@
+use oop_my_test::*;
+
+fn main() {
+    let mut p = Post::new();
+
+    p.request_review();
+}
