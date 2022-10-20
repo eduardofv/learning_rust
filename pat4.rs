84f928a581ec29203a25adb58ab76796e6ffda1d
Eduardo Flores <eduardofv@gmail.com>
2022-10-20 17:23:46 -0500

    18.3.3 moves in patterns

---
+++ pat4.rs
@@ -0,0 +1,14 @@
+fn foo(x: String, _: String) {
+    println!("moving {x} ");
+}
+
+fn main() {
+    let x = String::from("a");
+    let y = String::from("b");
+
+    println!("{x} {y}");
+    foo(x, y);
+    //println!("{x}"); x was moved into fn
+    //println!("{y}"); y was also moved despite not used
+}
+
