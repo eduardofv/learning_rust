cdb6a01a71c9bfaa720735ed23b040d39744ee7f
Eduardo Flores <eduardofv@gmail.com>
2022-10-26 18:09:28 -0500

    20.2.3 workers almost finished

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,7 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex}, 
+    thread,
+};
 
 pub struct Worker {
     id: u32,
@@ -6,17 +9,23 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: u32) -> Worker {
+    fn new(
+            id: u32, 
+            receiver: Arc<Mutex<mpsc::Receiver<Job>>>
+        ) -> Worker {
         Worker {
             id: id,
-            handle: thread::spawn(|| {}),
+            handle: thread::spawn(|| { receiver; }),
         }
     }
 }
 
 pub struct ThreadPool {
     threads: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(num: usize) -> ThreadPool {
@@ -24,18 +33,24 @@ impl ThreadPool {
 
         let mut threads = Vec::with_capacity(num);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         for id in 0..num {
-            threads.push(Worker::new(id as u32));
+            threads.push(Worker::new(id as u32, Arc::clone(&receiver)));
         }
 
-        ThreadPool { threads }
+        ThreadPool { threads, sender }
     }
 
     pub fn execute<F>(&self, handler: F) 
     where
         F: FnOnce() -> () + Send + 'static,
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap;
     }
 }
 
